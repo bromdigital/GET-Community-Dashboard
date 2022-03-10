0a7653dff00bdfd0a715ad96dcd19a504f49dcdd
Simon Bromfield <simon@simonbromfield>
2022-03-10 16:14:41 +0000

    changed width of recent mints

--- frontend/routes/getUsed.js
+++ frontend/routes/getUsed.js
@@ -15,27 +15,27 @@ const generateMints = async () => {
 
     const recentMints = await subGraph.recentMints(30)
 
-    let html = `<div class="container clearfix">
-        <div class="container-fluid py-4">
-            <div class="row newMints">`
+    let html = `<div id="mintContainer">
+    <div class="container-fluid py-4">
+        <div class="row newMints">`
 
     for (let i = 0; i < recentMints.firstFour.length; i++) {
       html += `<div class="col-xl-3 col-sm-6 mb-xl-0 mb-4">
-            <div class="card">
-                <div class="card-body p-3">
-                    <div class="row">
-                        <div class="numbers">
-                            <p class="text-sm mb-0">
+      <div class="card">
+          <div class="card-body p-3">
+              <div class="row">
+                  <div class="numbers">
+                      <p class="text-sm mb-0">
                                 ${recentMints.firstFour[i].blockTimestamp}
-                            </p>
-                            <h5>
-                            <a href="../event-profile/${recentMints.firstFour[i].event.id}">
-                                ${helpers.truncate(recentMints.firstFour[i].event.eventName, 20)}</a><br />
+                                </p>
+                                <h5>
+                                <a href="../event-profile/${recentMints.firstFour[i].event.id}">
+                                ${helpers.truncate(recentMints.firstFour[i].event.eventName, 20)}<br /></a>
                                 <a href="../ticketeer/${recentMints.firstFour[i].event.ticketeerName}">
                                 <span class="text-sm font-weight-bolder">${recentMints.firstFour[i].event.ticketeerName}</span></a>
-                            </h5>
-                        </div>
-                        <div class="imgTile" style="background-image: url('${recentMints.firstFour[i].event.imageUrl}'" );>
+                                </h5>
+                                </div>
+                                <div class="imgTile" style="background-image: url(' ${recentMints.firstFour[i].event.imageUrl}'" );>
                         </div>
                         <h6>
                             ${Number(recentMints.firstFour[i].getDebitedFromSilo).toFixed(6)} $GET used as fuel.
@@ -46,24 +46,22 @@ const generateMints = async () => {
         </div>`
     }
 
-    html += `</tbody>
-        </table>
-    </div>
-</div>
-<div class="col_full nobottommargin">
-<div class="table-responsive">
-    <table class="table table-striped">
-        <thead>
-            <tr>
-                <th scope="col">Ticket Number</th>
-                <th scope="col">Event Name</th>
-                <th scope="col">Ticketeer Name</th>
-                <th scope="col">$GET used</th>
-                <th scope="col">Block Timestamp</th>
-                <th scope="col">NFT Explorer</th>
-            </tr>
-        </thead>
-        <tbody>                   
+    html += `</div>
+
+    <div class="col_full nobottommargin">
+        <div class="table-responsive">
+            <table class="table table-striped">
+                <thead>
+                    <tr>
+                        <th scope="col">Ticket Number</th>
+                        <th scope="col">Event Name</th>
+                        <th scope="col">Ticketeer Name</th>
+                        <th scope="col">$GET used</th>
+                        <th scope="col">Block Timestamp</th>
+                        <th scope="col">NFT Explorer</th>
+                    </tr>
+                </thead>
+                <tbody>
         `
     for (let ii = 0; ii < recentMints.recentMints.length; ii++) {
       html += `<tr>
@@ -95,7 +93,6 @@ const generateMints = async () => {
 
     html += `</tbody>
         </table>
-    </div>
 </div>`
 
     return {
