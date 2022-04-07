d8125779faea928967e0f391329d110f1f8f5cea
Simon Bromfield <simon@simonbromfield>
2022-04-07 21:46:40 +0100

    further touches to charts page

--- frontend/controllers/chartsController.js
+++ frontend/controllers/chartsController.js
@@ -1,11 +1,24 @@
 // include the functions
 const subGraph = require('../inc/subGraph')
+const dailyStats = require('../services/dailyStats')
+const helpers = require('../inc/helpers')
 
-const charts = (req, res, next) => {
-  const locals = {
-    pageTitle: 'GET Community - Charts'
+const charts = async (req, res, next) => {
+  try {
+    const todayGET = await dailyStats.getTodayUsage()
+
+    const locals = {
+      pageTitle: 'GET Community - Charts',
+      helpers: helpers,
+      todayGET: {
+        getDebitedFromSilos: todayGET[0].getDebitedFromSilos,
+        mintCount: todayGET[0].ticketsToday
+      }
+    }
+    res.render('charts/home', locals)
+  } catch (error) {
+    res.status(400).send(error.message)
   }
-  res.render('charts/home', locals)
 }
 
 const chartData = async (req, res, next) => {
